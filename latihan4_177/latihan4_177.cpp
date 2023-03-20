8776cc3c9a5db559b9e7e10645694056e3a14578
fasyaaa <muhammad.a.f.2210@gmail.com>
2023-03-20 15:54:46 +0700

    menambahkan float

--- latihan4_177/latihan4_177.cpp
+++ latihan4_177/latihan4_177.cpp
@@ -1,7 +1,15 @@
-// latihan4_177.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using namespace std;
+
+float LuasPersegi(float p, float l) {
+    return p * l;
+}
+float LuasLingkaran(float r) {
+    return 3.14 * r * r;
+}
+float LuasSegitiga(float a, float t) {
+    return 0.5 * a * t;
+}
 
 int main()
 {
